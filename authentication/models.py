56c02ec7c4245fbe3787b506e65f654405095959
fbastier <francois.bastier@gmail.com>
2022-06-29 15:41:14 +0200

    1.1 User personnalisé

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    CREATOR = 'CREATOR'
+    SUBSCRIBER = 'SUBSCRIBER'
+    ROLE_CHOICES = (
+        (CREATOR, 'Créateur'),
+        (SUBSCRIBER, 'Abonné')
+    )
+    profil_photo = models.ImageField(verbose_name='Photo du Profil')
+    role = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name='Rôle')
--- fotoblog/settings.py
+++ fotoblog/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Ajout du modèle User personnalisé.
+AUTH_USER_MODEL = 'authentication.User'
